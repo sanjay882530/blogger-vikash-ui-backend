66516def8fae8b7dd254baa57056f816464dc7f1
sanjayhashcash <anuragcse611@gmail.com>
2024-09-14 22:05:51 +0530

    changes and added cors IP v1.3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 const VITE_BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "https://vikashblog.up.railway.app";
+  process.env.VITE_BACKEND_URL || "https://vikashblog.up.railway.app";
 
 // https://vitejs.dev/config/
 export default defineConfig({

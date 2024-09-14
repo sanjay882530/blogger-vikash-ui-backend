64904ac63a86814056ffca4d39b83f1d14db2910
sanjayhashcash <anuragcse611@gmail.com>
2024-09-14 23:14:18 +0530

    Remove config file

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   MDBContainer,
   MDBInput,
@@ -7,14 +7,13 @@ import {
   MDBIcon,
   MDBCard,
 } from "mdb-react-ui-kit";
-import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import UserContext from "../context/UserContext";
-import dotenv from "dotenv";
-dotenv.config();
+
+// Use import.meta.env to access environment variables in Vite
 const VITE_URL =
-  process.env.VITE_BACKEND_URL || "https://vikashblog.up.railway.app";
+  import.meta.env.VITE_BACKEND_URL || "https://vikashblog.up.railway.app";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -24,12 +23,12 @@ function Login() {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
   const [isSubmitting] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setUser(username);
     try {
       const response = await axios.post(`${VITE_URL}/api/login`, {
-        // const response = await axios.post("https://vikashblog.up.railway.app/api/login", {
         username,
         password,
       });
@@ -62,95 +61,93 @@ function Login() {
   };
 
   return (
-    <>
-      <MDBContainer className="p-3 my-5 d-flex flex-column align-items-center">
-        <MDBCard className="p-4 w-100" style={{ maxWidth: "500px" }}>
-          <h3 style={{ textAlign: "center" }}>Welcome to Blogger Login</h3>
-          <form onSubmit={handleSubmit}>
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Username"
-              id="form1"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+    <MDBContainer className="p-3 my-5 d-flex flex-column align-items-center">
+      <MDBCard className="p-4 w-100" style={{ maxWidth: "500px" }}>
+        <h3 style={{ textAlign: "center" }}>Welcome to Blogger Login</h3>
+        <form onSubmit={handleSubmit}>
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Username"
+            id="form1"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form2"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div className="d-flex justify-content-between mx-3 mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              value=""
+              id="flexCheckDefault"
+              label="Remember me"
             />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Password"
-              id="form2"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <div className="d-flex justify-content-between mx-3 mb-4">
-              <MDBCheckbox
-                name="flexCheck"
-                value=""
-                id="flexCheckDefault"
-                label="Remember me"
-              />
-              <a href="#!">Forgot password?</a>
-            </div>
-            <MDBBtn className="w-100 mb-4" type="submit">
-              Sign in
-            </MDBBtn>
-            {message.text && (
-              <p style={{ color: message.color }}>{message.text}</p>
-            )}
-            <div className="text-center">
-              <p>
-                Not a member? <Link to="/signup">Register</Link>
-              </p>
-              <p>or sign up with:</p>
-              <div
-                className="d-flex justify-content-between mx-auto"
-                style={{ width: "60%" }}
+            <a href="#!">Forgot password?</a>
+          </div>
+          <MDBBtn className="w-100 mb-4" type="submit">
+            Sign in
+          </MDBBtn>
+          {message.text && (
+            <p style={{ color: message.color }}>{message.text}</p>
+          )}
+          <div className="text-center">
+            <p>
+              Not a member? <Link to="/signup">Register</Link>
+            </p>
+            <p>or sign up with:</p>
+            <div
+              className="d-flex justify-content-between mx-auto"
+              style={{ width: "60%" }}
+            >
+              <MDBBtn
+                disabled={isSubmitting}
+                pointer-events="none"
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
               >
-                <MDBBtn
-                  disabled={isSubmitting}
-                  pointer-events="none"
-                  tag="a"
-                  color="none"
-                  className="m-1"
-                  style={{ color: "#1266f1" }}
-                >
-                  <MDBIcon fab icon="facebook-f" size="sm" />
-                </MDBBtn>
-                <MDBBtn
-                  disabled={isSubmitting}
-                  tag="a"
-                  color="none"
-                  className="m-1 "
-                  style={{ color: "#1266f1" }}
-                >
-                  <MDBIcon fab icon="twitter" size="sm" />
-                </MDBBtn>
-                <MDBBtn
-                  disabled={isSubmitting}
-                  tag="a"
-                  color="none"
-                  className="m-1"
-                  style={{ color: "#1266f1" }}
-                >
-                  <MDBIcon fab icon="google" size="sm" />
-                </MDBBtn>
-                <MDBBtn
-                  disabled={isSubmitting}
-                  tag="a"
-                  color="none"
-                  className="m-1"
-                  style={{ color: "#1266f1" }}
-                >
-                  <MDBIcon fab icon="github" size="sm" />
-                </MDBBtn>
-              </div>
+                <MDBIcon fab icon="facebook-f" size="sm" />
+              </MDBBtn>
+              <MDBBtn
+                disabled={isSubmitting}
+                tag="a"
+                color="none"
+                className="m-1 "
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="twitter" size="sm" />
+              </MDBBtn>
+              <MDBBtn
+                disabled={isSubmitting}
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="google" size="sm" />
+              </MDBBtn>
+              <MDBBtn
+                disabled={isSubmitting}
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="github" size="sm" />
+              </MDBBtn>
             </div>
-          </form>
-        </MDBCard>
-      </MDBContainer>
-    </>
+          </div>
+        </form>
+      </MDBCard>
+    </MDBContainer>
   );
 }
 
